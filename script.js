1f7dc404a843e7550d9ecda00174259dd8b8df18
Jessica Dao <xjessicadao@gmail.com>
2021-02-22 15:24:21 -0800

    questions added

---
+++ script.js
@@ -0,0 +1,62 @@
+var questions = [
+    {
+    question: `What is the largest country in the world?`,
+        choiceA: "Canada",
+        choiceB: "China",
+        choiceC: "Russia",
+    correct: "C",
+    }, {
+    question: `What is the 100th digit of Pi?`,
+        choiceA: "9",
+        choiceB: "5",
+        choiceC: "3",
+    correct: "A",
+    }, {
+    question: `What year did World War I begin?`,
+        choiceA: "1919",
+        choiceB: "1926",
+        choiceC: "1914",
+    correct: "C",
+    }, {
+    question: `Which of these countries is NOT a part of the Asian continent?`,
+        choiceA: "Russia",
+        choiceB: "Suriname",
+        choiceC: "Georgia",
+    correct: "B",
+    }, {
+    question: `Which is the largest freshwater lake in the world?`,
+        choiceA: "Lake Superior",
+        choiceB: "Caspian Sea",
+        choiceC: "Lake Michigan",
+    correct: "Caspian Sea",
+    }, {
+    question: `What is the scientific name of the knee cap?`,
+        choiceA: "Foramen Magnum",
+        choiceB: "Scapula",
+        choiceC: "Patella",
+    correct: "C",
+    }, {
+    question: `What is the fastest land animal?`,
+        choiceA: "Cheetah",
+        choiceB: "Lion",
+        choiceC: "Patella",
+    correct: "C",
+    }, {
+    question: `What is the Italian word for "Tomato"?`,
+        choiceA: "Cipolla",
+        choiceB: "Pomodoro",
+        choiceC: "Peperoncino",
+    correct: "B",
+    }, {
+    question: `Which of these fruits is NOT a berry?`,
+        choiceA: "Blueberry",
+        choiceB: "Banana",
+        choiceC: "Strawberry",
+    correct: "C",
+    }, {
+    question: `What is the oldest Disney Film?`,
+        choiceA: "Pinocchio",
+        choiceB: "Snow White",
+        choiceC: "Dumbo",
+    correct: "B"
+    }];
